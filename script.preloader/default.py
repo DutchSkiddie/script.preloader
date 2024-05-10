83dc82f409bf74a40f869c4b56a72c5d27425aa9
DutchGreen <39133917+DutchSkiddie@users.noreply.github.com>
2024-05-10 23:53:32 +0200

    Removed

--- script.preloader/default.py
+++
@@ -1,8 +0,0 @@
-from resources.lib import dialog
-from resources.lib import setup  
-
-
-if (__name__ == '__main__'):    
-    setupchoice = dialog.Welcome()
-    setup.Setup(setupchoice)
-    exit()
